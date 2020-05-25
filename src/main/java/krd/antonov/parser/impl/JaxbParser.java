b9ef22df2f2c5cb76d69cf8021349e34fa85fc54
Vladimir <v.antonov93@gmail.com>
2020-05-25 18:43:32 +0300

    add logging to JaxbParser create JaxbParserTest

--- src/main/java/krd/antonov/parser/impl/JaxbParser.java
+++ src/main/java/krd/antonov/parser/impl/JaxbParser.java
@@ -1,6 +1,7 @@
 package krd.antonov.parser.impl;
 
 import krd.antonov.parser.Parser;
+import org.apache.log4j.Logger;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -10,10 +11,13 @@ import java.io.File;
 
 public class JaxbParser implements Parser {
 
+    private static final Logger logger = Logger.getLogger(JaxbParser.class);
+
     @SuppressWarnings("rawtypes")
     public Object getObject(File file, Class clazz) throws JAXBException {
         JAXBContext context = JAXBContext.newInstance(clazz);
         Unmarshaller unmarshaller = context.createUnmarshaller();
+        logger.info("object " + clazz.getSimpleName() + " is read from " + file.getAbsolutePath());
         return unmarshaller.unmarshal(file);
     }
 
@@ -21,5 +25,6 @@ public class JaxbParser implements Parser {
         JAXBContext context = JAXBContext.newInstance(obj.getClass());
         Marshaller marshaller = context.createMarshaller();
         marshaller.marshal(obj, file);
+        logger.info("object" + obj.getClass().getSimpleName() + " is saved to " + file.getAbsolutePath());
     }
 }
---
+++ src/test/java/krd/antonov/parser/impl/JaxbParserTest.java
@@ -0,0 +1,10 @@
+package krd.antonov.parser.impl;
+
+import krd.antonov.parser.Parser;
+
+import java.io.File;
+
+public class JaxbParserTest {
+    private Parser parser;
+    private File file;
+}
