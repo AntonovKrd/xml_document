0fc2f231c7a4092ce53c6556303cdf4b91e70e40
Vladimir <v.antonov93@gmail.com>
2020-05-25 20:02:55 +0300

    add new entity class Members.java

--- src/main/java/krd/antonov/entity/Members.java
+++ src/main/java/krd/antonov/entity/Members.java
@@ -1,4 +1,59 @@
 package krd.antonov.entity;
 
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlRootElement(name = "members")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Members {
+
+    @XmlElement
+    private List<Member> members = new ArrayList<>();
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(propOrder = {"name", "roles"})
+    public static class Member {
+
+        @XmlAttribute
+        private String name;
+
+        @XmlElement
+        private List<Role> roles = new ArrayList<>();
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<Role> getRoles() {
+            return roles;
+        }
+
+        public void setRoles(List<Role> roles) {
+            this.roles = roles;
+        }
+    }
+
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(propOrder = {"name", "project"})
+    public static class Role {
+
+        @XmlAttribute
+        private String name;
+
+        @XmlAttribute
+        private String project;
+    }
 }
